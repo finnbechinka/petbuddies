108c1af63e76d64d2fad2bd4e93a9d35adfc9fee
Finn Bechinka <finn@bechinka.eu>
2021-11-19 00:33:34 +0100

    added name attribute to list

--- server/models/List.js
+++ server/models/List.js
@@ -1,7 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
 
     const BuddyList = sequelize.define("BuddyList", {
-        
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     });
 
     return BuddyList;
